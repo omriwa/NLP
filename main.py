b98c19f7137e4105f5a1608d383bd1bfe1245640
omri wallach <omriwallach1992@gmail.com>
2021-01-19 17:41:07 +0100

    training

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
+# Natural Language Processing
+
+# Importing the libraries
 import numpy as np
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 import pandas as pd
 
-# Dataset
-dataset = pd.read_csv("Restaurant_Reviews.tsv",delimiter="\t",quoting=3)
+# Importing the dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
 
-# Cleaning the dataset
+# Cleaning the texts
 import re
 import nltk
-nltk.download("stopwords")
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-
 corpus = []
-
 for i in range(0, 1000):
-    review = re.sub('[^a-zA-Z]', ' ', dataset["Review"][i])
-    review.lower()
-    review.split()
-    ps = PorterStemmer()
-    review = [ps.stem(word) for word in review if not word in set(stopwords.words("english"))]
-    review = ' '.join(review)
-    corpus.append(review)
+  review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+  review = review.lower()
+  review = review.split()
+  ps = PorterStemmer()
+  all_stopwords = stopwords.words('english')
+  all_stopwords.remove('not')
+  review = [ps.stem(word) for word in review if not word in set(all_stopwords)]
+  review = ' '.join(review)
+  corpus.append(review)
+
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, -1].values
+
+# Splitting the data
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+# Training the model
+from sklearn.naive_bayes import GaussianNB
+
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
